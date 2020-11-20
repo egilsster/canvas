eaa2a30960d495a004e9bd9afcee9266dd10ae90
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2020-11-20 09:26:04 +0100

    chore: update all deps

--- src/models/shape.ts
+++ src/models/shape.ts
@@ -3,7 +3,7 @@ import Point from './point';
 export default abstract class Shape {
   private _position: Point;
 
-  constructor(x: number, y: number, public color) {
+  constructor(x: number, y: number, public color: string) {
     this._position = new Point(x, y);
   }
 
--- webpack/rules.js
+++ webpack/rules.js
@@ -22,18 +22,14 @@ module.exports = [
   },
   {
     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+    loader: 'url-loader',
+    options: {
+      limit: 10000,
+      mimetype: 'application/font-woff',
+    },
   },
   {
     test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
     loader: 'file-loader',
-    // }, {
-    //     test: '\.jpg$',
-    //     exclude: /node_modules/,
-    //     loader: 'file-loader'
-    // }, {
-    //     test: '\.png$',
-    //     exclude: /node_modules/,
-    //     loader: 'url-loader'
   },
 ];
