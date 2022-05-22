2ac05cd560917fc15ecb979e468a8acd97c8145a
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2022-05-22 20:23:09 +0200

    refactor: design tweaks

--- src/utils/resizer.ts
+++ src/utils/resizer.ts
@@ -1,9 +1,29 @@
-export default function ResizeCanvas(canvasElement: HTMLCanvasElement): void {
+function calcCanvasWidth(containerEl: HTMLDivElement): {
+  width: number;
+  height: number;
+} {
+  const style = window.getComputedStyle(containerEl);
+  const width = containerEl.offsetWidth;
+  const height = containerEl.offsetHeight;
+
+  const margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+  const padding =
+    parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
+  const border =
+    parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
+
+  const totalAvailableHeight = height - margin - padding - border;
+  const totalAvailableWidth = width - margin - padding - border;
+
+  return { width: totalAvailableWidth, height: totalAvailableHeight };
+}
+
+export default function ResizeCanvas(canvasEl: HTMLCanvasElement): void {
   const container = document.querySelector(
     "#canvas-container"
   ) as HTMLDivElement;
-  const canvas = canvasElement;
 
-  canvas.setAttribute("width", String(container.clientWidth));
-  canvas.setAttribute("height", String(container.clientHeight));
+  const { width, height } = calcCanvasWidth(container);
+  canvasEl.setAttribute("width", String(width));
+  canvasEl.setAttribute("height", String(height));
 }
